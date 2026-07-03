a309d929f8b00306988669af565b8b1f8fee6e0c
Ernesto Afane <ernesto@97thfloor.com>
2026-07-03 08:51:21 -0600

    build problems repaired

---
+++ src/icons/index.tsx
@@ -0,0 +1,115 @@
+import { SVGProps } from "react";
+
+export const CalenderIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M7.5 3.25a.75.75 0 0 1 .75.75v.75h7.5V4a.75.75 0 0 1 1.5 0v.75h.75A2.75 2.75 0 0 1 20.75 7.5v10A2.75 2.75 0 0 1 18 20.25H6A2.75 2.75 0 0 1 3.25 17.5v-10A2.75 2.75 0 0 1 6 4.75h.75V4a.75.75 0 0 1 .75-.75ZM4.75 9.5v8a1.25 1.25 0 0 0 1.25 1.25h12a1.25 1.25 0 0 0 1.25-1.25v-8H4.75Zm14.5-1.5V7.5A1.25 1.25 0 0 0 18 6.25H6A1.25 1.25 0 0 0 4.75 7.5V8h14.5Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export const ChevronDownIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M5.47 8.47a.75.75 0 0 1 1.06 0L12 13.94l5.47-5.47a.75.75 0 1 1 1.06 1.06l-6 6a.75.75 0 0 1-1.06 0l-6-6a.75.75 0 0 1 0-1.06Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export const EyeIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M10 3.75c-4.06 0-6.99 3.09-8.16 4.62a1.7 1.7 0 0 0 0 2.26C3.01 12.16 5.94 15.25 10 15.25s6.99-3.09 8.16-4.62a1.7 1.7 0 0 0 0-2.26C16.99 6.84 14.06 3.75 10 3.75Zm0 8.5a3.25 3.25 0 1 1 0-6.5 3.25 3.25 0 0 1 0 6.5Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export const EyeCloseIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M3.28 2.22a.75.75 0 0 0-1.06 1.06l14.5 14.5a.75.75 0 1 0 1.06-1.06l-1.98-1.98c1.4-1.05 2.55-2.36 3.26-3.36a1.7 1.7 0 0 0 0-2.26C17.87 7.34 14.94 4.25 10.88 4.25c-1.44 0-2.72.39-3.83.98L3.28 2.22ZM8.5 7.44l1.32 1.32a2.25 2.25 0 0 1 2.42 2.42l1.32 1.32a3.75 3.75 0 0 0-5.06-5.06Zm-3.7-.86 1.14 1.14C4.6 8.6 3.61 9.68 2.94 10.5a1.7 1.7 0 0 0 0 2.26c1.17 1.53 4.1 4.62 8.16 4.62 1.17 0 2.24-.26 3.18-.66l-1.28-1.28a5.62 5.62 0 0 1-1.9.32c-3.28 0-5.78-2.55-6.79-3.87a.2.2 0 0 1 0-.26c.5-.65 1.3-1.53 2.32-2.29l-.03.02Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export const TimeIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12 3.25a8.75 8.75 0 1 0 0 17.5 8.75 8.75 0 0 0 0-17.5ZM1.75 12a10.25 10.25 0 1 1 20.5 0 10.25 10.25 0 0 1-20.5 0Zm10.25-5.75a.75.75 0 0 1 .75.75v4.69l3.03 3.03a.75.75 0 1 1-1.06 1.06l-3.25-3.25a.75.75 0 0 1-.22-.53V7a.75.75 0 0 1 .75-.75Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export const EnvelopeIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M3.33 4.17h13.34c.73 0 1.33.6 1.33 1.33v9.17c0 .73-.6 1.33-1.33 1.33H3.33c-.73 0-1.33-.6-1.33-1.33V5.5c0-.73.6-1.33 1.33-1.33Z"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="m2.5 5.83 7.5 5.42 7.5-5.42"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
